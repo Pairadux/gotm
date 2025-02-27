546f1012122554b20e060075d9d19f15cc9ce65c
Pairadux <austincgause@gmail.com>
2025-02-26 21:21:31 -0500

    Add storage package

---
+++ internal/storage/jsonstore.go
@@ -0,0 +1,54 @@
+package storage
+
+// IMPORTS {{{
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/Pairadux/gotm/internal/models"
+	"github.com/spf13/viper"
+) // }}}
+
+func PrintJson(allTasks *models.TaskList) {
+	data, err := json.MarshalIndent(allTasks, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
+}
+
+func ToJson(allTasks *models.TaskList) []byte {
+	data, err := json.Marshal(allTasks)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func SaveTasksToFile(filename string, allTasks *models.TaskList) error {
+	return os.WriteFile(filename, ToJson(allTasks), 0o644)
+}
+
+func LoadTasks() (*models.TaskList, error) {
+	filename := viper.GetString("json_path")
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := models.TaskList{}
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, err
+	}
+
+	return &tasks, nil
+}
